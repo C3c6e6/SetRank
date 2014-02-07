21fcb566b705e8ab3996713d4c42193b63a9c790
Cedric Simillion <cedric.simillion@dkf.unibe.ch>
2014-02-07 15:34:37 +0100

    * Moved the createIDConverter function from the OmicsTools package to SetRank.

--- R/buildSets.R
+++ R/buildSets.R
@@ -176,4 +176,30 @@ minimalI <- function(m,s,g, maximalP) {
 	minimalI = which(pValues < maximalP)[1] - 1
 }
 
+createIDConverter <- function(annotationPackageName, from, to) {
+	require(annotationPackageName, character.only=TRUE)
+	keySet = keys(eval(parse(text=annotationPackageName)), from)
+	conversionTable = select(eval(parse(text=annotationPackageName)), 
+			keys=keySet, keytype=from, columns=to)
+	conversion = as.list(by(conversionTable, as.factor(conversionTable[[from]]), 
+					function(t) unique(t[[to]]), simplify=FALSE))
+	outputFunction = function(x, na.rm=TRUE, drop.ambiguous=FALSE) {
+		knownX = unique(x)
+		outputList = as.list(rep(NA, length(knownX)))
+		names(outputList) = knownX
+		knownX = intersect(knownX, names(conversion))
+		if (drop.ambiguous) {
+			ambiguous = sapply(conversion, function(x) length(x) > 1)
+			knownX = setdiff(knownX, names(conversion[ambiguous]))
+		}
+		outputList[knownX] = conversion[knownX]
+		output = unlist(outputList[x], use.names=FALSE)
+		if (na.rm) {
+			output = output[!is.na(output)]
+		}
+		output
+	} 
+	return(outputFunction)
+}
+
 

0ea0325fb3a3da920ea6dfe011015d6abf87dbb8
Cedric Simillion <cedric.simillion@dkf.unibe.ch>
2015-12-08 13:30:24 +0100

    Reduced the size of the example dataset to speed up the example code, to satisfy the requirements of CRAN.

--- R/buildSets.R
+++ R/buildSets.R
@@ -98,11 +98,11 @@ expandWithTermOffspring <- function(subTable, tableSplit, offspringList) {
 #'     together with the p-value.}  
 #' }
 #' @examples
-#' referenceSet = sprintf("gene_%03d", 1:100)
-#' geneSets = lapply(1:9, function(i) sample(referenceSet[((i-1)*10):((i+1)*10)], 10))
-#' annotationTable = data.frame(termID=sprintf("set_%02d", rep(1:9, each=10)), 
+#' referenceSet = sprintf("gene_%02d", 1:50)
+#' geneSets = lapply(1:9, function(i) sample(referenceSet[((i-1)*5):((i+1)*5)], 5))
+#' annotationTable = data.frame(termID=sprintf("set_%02d", rep(1:9, each=5)), 
 #'         geneID=unlist(geneSets),
-#'         termName = sprintf("dummy gene set %d", rep(1:9, each=10)),
+#'         termName = sprintf("dummy gene set %d", rep(1:9, each=5)),
 #'         dbName = "dummyDB",
 #'         description = "A dummy gene set DB for testing purposes")
 #' collection = buildSetCollection(annotationTable, referenceSet=referenceSet)
